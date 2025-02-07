2b1d1b317056dd2971d969b3d1cacc3508b19604
Doğuş Yaman <yamandogus251@gmail.com>
2025-02-07 23:29:59 +0300

    Add slick-carousel CSS imports for slider functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Input } from "@nextui-org/input";
@@ -8,6 +7,19 @@ import { Select, SelectItem } from "@nextui-org/select";
 import { WeatherData } from "@/types/type";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import {
+  cloud,
+  hot,
+  mist,
+  overcast,
+  rain,
+  snow,
+  sun,
+  thunderstorm,
+  wind,
+} from "@/data/weater";
 
 const URL =
   process.env.NEXT_PUBLIC_API_URL ||
@@ -26,19 +38,6 @@ const settings = {
   slidesToShow: 1,
   slidesToScroll: 1,
 };
-
-const sun =
-  "https://lottie.host/ec07c040-7e69-4d35-a0fd-80f8029bef91/KB41wLx7f1.lottie";
-const rain =
-  "https://lottie.host/5e3ade54-47b5-4f66-9338-4af14a6f3d4d/fGGAv0CbTv.lottie";
-const cloud =
-  "https://lottie.host/11f541ca-ccf0-4523-90e7-d3b4fad9011c/WrMOXg0Fud.lottie";
-const overcast =
-  "https://lottie.host/0bfe7cc7-3417-45cd-8dfb-ee89194af150/EFRK8B5sEf.lottie";
-const snow =
-  "https://lottie.host/521c3e53-4210-44b6-acd7-ae8f11fc11cf/QqcTwi6dNd.lottie";
-const wind =
-  "https://lottie.host/0bfe7cc7-3417-45cd-8dfb-ee89194af150/EFRK8B5sEf.lottie";
 
 export default function Home() {
   const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
@@ -97,7 +96,7 @@ export default function Home() {
 
   return (
     <div>
-      <div className="grid md:grid-cols-2 xs:grid-cols-1 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
         <div>
           <Input
             className="bg-dark-900"
@@ -133,7 +132,8 @@ export default function Home() {
           </Select>
         </div>
       </div>
-      <div className="mt-4 xs:hidden md:block">
+
+      <div className="mt-4 hidden sm:block">
         {weatherData && (
           <div className="flex flex-wrap justify-center align-center gap-4 mt-4">
             {weatherData.list
@@ -169,7 +169,14 @@ export default function Home() {
                                       ? overcast
                                       : item.weather[0].main === "Wind"
                                         ? wind
-                                        : ""
+                                        : item.weather[0].main ===
+                                            "Thunderstorm"
+                                          ? thunderstorm
+                                          : item.weather[0].main === "Mist"
+                                            ? mist
+                                            : item.weather[0].main === "Hot"
+                                              ? hot
+                                              : ""
                           }
                           loop
                           autoplay
@@ -203,82 +210,79 @@ export default function Home() {
           </div>
         )}
       </div>
-      <div className="mt-4 xs:block md:hidden">
+      <div className="mt-4 block sm:hidden">
         <h6>test xs screen</h6>
         {weatherData && (
-            <Slider {...settings}>
-          <div className="flex flex-wrap justify-center align-center gap-4 mt-4">
+          <Slider {...settings}>
             {weatherData.list
               .filter((item) => item.dt_txt.includes("12:00:00"))
               .map((item, index) => (
-                  <div
-                    key={index}
-                    className="flex justify-center align-center w-[199px]"
+                <div key={index} className="flex flex-row gap-4 w-[199px]">
+                  <Card
+                    className={`flex flex-col items-center justify-center `}
                   >
-                    <Card
-                      className={`flex flex-col items-center justify-center `}
-                    >
-                      <CardBody className="flex flex-col items-center justify-center">
-                        <h3 className={`${"text-lg"} font-semibold`}>
-                          {new Date(item.dt * 1000).toLocaleDateString(
-                            "tr-TR",
-                            {
-                              weekday: "long",
-                            }
-                          )}
-                        </h3>
-                        <div className="flex items-center justify-center">
-                          <DotLottieReact
-                            height={200}
-                            width={200}
-                            src={
-                              item.weather[0].main === "Clouds"
-                                ? cloud
-                                : item.weather[0].main === "Rain"
-                                  ? rain
-                                  : item.weather[0].main === "Snow"
-                                    ? snow
-                                    : item.weather[0].main === "Clear"
-                                      ? sun
-                                      : item.weather[0].main === "Overcast"
-                                        ? overcast
-                                        : item.weather[0].main === "Wind"
-                                          ? wind
-                                          : ""
-                            }
-                            loop
-                            autoplay
-                          />
-                        </div>
-                        <p className={`${"text-lg"} font-semibold capitalize`}>
-                          {item.weather[0].description}
-                        </p>
-                        <h6
-                          className={`text-[#FFB457] font-[800] ${"text-[30px]"} font-semibold`}
-                        >
-                          {item.main.temp}°C
-                        </h6>
+                    <CardBody className="flex flex-col items-center justify-center">
+                      <h3 className={`${"text-lg"} font-semibold`}>
+                        {new Date(item.dt * 1000).toLocaleDateString("tr-TR", {
+                          weekday: "long",
+                        })}
+                      </h3>
+                      <div className="flex items-center justify-center">
+                        <DotLottieReact
+                          height={200}
+                          width={200}
+                          src={
+                            item.weather[0].main === "Clouds"
+                              ? cloud
+                              : item.weather[0].main === "Rain"
+                                ? rain
+                                : item.weather[0].main === "Snow"
+                                  ? snow
+                                  : item.weather[0].main === "Clear"
+                                    ? sun
+                                    : item.weather[0].main === "Overcast"
+                                      ? overcast
+                                      : item.weather[0].main === "Wind"
+                                        ? wind
+                                        : item.weather[0].main ===
+                                            "Thunderstorm"
+                                          ? thunderstorm
+                                          : item.weather[0].main === "Mist"
+                                            ? mist
+                                            : item.weather[0].main === "Hot"
+                                              ? hot
+                                              : ""
+                          }
+                          loop
+                          autoplay
+                        />
+                      </div>
+                      <p className={`${"text-lg"} font-semibold capitalize`}>
+                        {item.weather[0].description}
+                      </p>
+                      <h6
+                        className={`text-[#FFB457] font-[800] ${"text-[30px]"} font-semibold`}
+                      >
+                        {item.main.temp}°C
+                      </h6>
 
-                        <div className="mt-4 grid grid-cols-2 gap-4 text-center">
-                          <div>
-                            <p className="text-sm">Nem</p>
-                            <p className="font-semibold">
-                              {item.main.humidity}%
-                            </p>
-                          </div>
-                          <div>
-                            <p className="text-sm">Rüzgar</p>
-                            <p className="font-semibold">
-                              {item.wind.speed} km/s
-                            </p>
-                          </div>
+                      <div className="mt-4 grid grid-cols-2 gap-4 text-center">
+                        <div>
+                          <p className="text-sm">Nem</p>
+                          <p className="font-semibold">{item.main.humidity}%</p>
                         </div>
-                      </CardBody>
-                    </Card>
-                  </div>
+                        <div>
+                          <p className="text-sm">Rüzgar</p>
+                          <p className="font-semibold">
+                            {item.wind.speed} km/s
+                          </p>
+                        </div>
+                      </div>
+                    </CardBody>
+                  </Card>
+                </div>
               ))}
-          </div>
-              </Slider>
+          </Slider>
         )}
       </div>
     </div>
---
+++ data/weater.ts
@@ -0,0 +1,36 @@
+const sun =
+  "https://lottie.host/2f14a83d-2b99-4b7d-919f-10513bf9968e/rmCfXvx8RN.lottie";
+const rain =
+  "https://lottie.host/7ae23a7b-3002-4d66-abc9-af84f62bc016/O1rmQjOD3u.lottie";
+const cloud =
+  "https://lottie.host/7be3df85-9442-4fdc-a047-266801fefcd8/D6rzrEJrJ1.lottie";
+const overcast =
+  "https://lottie.host/ac0b5f5f-b992-4c27-8233-8fa9549aa112/QC2CtHCXhz.lottie";
+const snow =
+  "https://lottie.host/5fbb9d88-2209-49ab-9429-85ba19acadb0/a7GT89L3N2.lottie";
+const wind =
+  "https://lottie.host/142bf664-7337-49b3-a98f-7c003c0bbcf4/ra7bm6X0hi.lottie";
+const thunderstorm =
+  "https://lottie.host/c1dd06be-8923-4041-afd5-eb0068959f21/HBEJdNJsTI.lottie";
+const mist =
+  "https://lottie.host/c99850b7-983f-46a3-b153-4187b87e89a0/HOFa4Djh5T.lottie";
+const hot =
+  "https://lottie.host/ff6d950e-5bed-422f-a073-e35739a6eb30/8dnFIvfy9r.lottie";
+const partlyCloudy =
+  "https://lottie.host/5e3ade54-47b5-4f66-9338-4af14a6f3d4d/partly-cloudy.lottie";
+const showerRain =
+  "https://lottie.host/5e3ade54-47b5-4f66-9338-4af14a6f3d4d/shower-rain.lottie";
+
+export {
+  sun,
+  rain,
+  cloud,
+  overcast,
+  snow,
+  wind,
+  thunderstorm,
+  mist,
+  hot,
+  partlyCloudy,
+  showerRain,
+};
